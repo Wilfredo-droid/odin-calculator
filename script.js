f01bca201c8add67b48158541f4568e924285420
Wilfredo-droid <wilfredoandres10@gmail.com>
2023-12-25 17:05:58 -0400

    Use a small formula to create the beginning and end of rows

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ for(let i = 1; i <= 3; i++){
 
     calculatorRow.classList.add("row");
 
-    for(let j = 1; j <= 3; j++){
+    //(i * 3) - 2 Is the beginning of the row I.e 1,4,7 and i * 3 is the end of the row 3,6,7 I could have used variables with names to explain better but I wanted to feel smart 
+    
+    for(let j = ((i * 3) - 2); j <= i * 3; j++){
         let calculatorNumber = document.createElement("button");
         calculatorNumber.innerText = j;
         calculatorNumber.classList.add("number");
@@ -14,5 +16,6 @@ for(let i = 1; i <= 3; i++){
     }
   
     calculatorBody.appendChild(calculatorRow);
+    
 
 }
