737f56a86be634865ca48790248dd2ae94175cc0
Wilfredo-droid <wilfredoandres10@gmail.com>
2023-12-25 16:20:05 -0400

    Add inner loop to create numbers append to calculator body

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let calculatorBody = document.querySelector(".calculatorBody");
 
-for(let i = 0; i <= 3; i++){
+for(let i = 1; i <= 3; i++){
 
     let calculatorRow = document.createElement("div");
 
     calculatorRow.classList.add("row");
 
+    for(let j = 1; j <= 3; j++){
+        let calculatorNumber = document.createElement("button");
+        calculatorNumber.innerText = j;
+        calculatorNumber.classList.add("number");
+        calculatorRow.appendChild(calculatorNumber);
+    }
   
+    calculatorBody.appendChild(calculatorRow);
 
 }
